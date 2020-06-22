af265e260a62e7b6ae6021ed20c1f6638238418e
devAM100 <devismejia@gmail.com>
2020-06-22 18:04:36 -0400

    using nivo instead of victory pies

--- src/ProgramPie.js
+++ src/ProgramPie.js
@@ -1,21 +1,23 @@
 import React from 'react'
 import './App.css';
-import { VictoryPie, VictoryTooltip } from 'victory';
+import { ResponsivePieCanvas } from '@nivo/pie'
 
 const ProgramPie = props => {
-
   const program_data = props.programs.map(element => {
-    return { x: element[0], y: element[1] };
+    return { id: element[0], label: element[0], value: element[1] };
   })
 
-  console.log("props.programs>>", props.programs)
+  // console.log("props.programs>>", props.programs)
 
   return (
     <div className="program-pie">
       <h3>Programs</h3>
-      <VictoryPie
+      <ResponsivePieCanvas
         data={program_data}
-        colorScale={[
+        margin={{ top: 40, right: 200, bottom: 40, left: 80 }}
+        pixelRatio={2}
+        innerRadius={0.5}
+        colors={[
           "#FFF59D",
           '#F4511E',
           "#DCE775",
@@ -23,8 +25,33 @@ const ProgramPie = props => {
           "#00796B",
           "#006064"
         ]}
-
-      />
+        borderColor={{ from: 'color', modifiers: [ [ 'darker', 0.6 ] ] }}
+        radialLabelsSkipAngle={10}
+        radialLabelsTextXOffset={6}
+        radialLabelsTextColor="#333333"
+        radialLabelsLinkOffset={0}
+        radialLabelsLinkDiagonalLength={16}
+        radialLabelsLinkHorizontalLength={24}
+        radialLabelsLinkStrokeWidth={1}
+        radialLabelsLinkColor={{ from: 'color' }}
+        slicesLabelsSkipAngle={10}
+        slicesLabelsTextColor="#333333"
+        animate={true}
+        motionStiffness={90}
+        motionDamping={15}
+        legends={[
+            {
+                anchor: 'left',
+                direction: 'column',
+                translateX: -50,
+                itemWidth: 60,
+                itemHeight: 14,
+                itemsSpacing: 2,
+                symbolSize: 14,
+                symbolShape: 'circle'
+            }
+        ]}
+    />
 
     </div>
   );
--- src/RaceEthnicityPie.js
+++ src/RaceEthnicityPie.js
@@ -1,26 +1,31 @@
 import React from 'react'
 import './App.css';
-// import { VictoryPie, VictoryChart, VictoryLegend } from 'victory';
 import { ResponsivePieCanvas } from '@nivo/pie'
 
 const RaceEthnicityPie = props => {
 
   const race_ethnicity_data = props.race_ethnicity.map(element => {
-    return { id: element[0], value: element[1] };
+    return { id: element[0], label: element[0], value: element[1] };
   })
 
   console.log("race_ethnicity_data>>", race_ethnicity_data)
 
   return (
-    <div>
+    <div className="race-ethnicity-pie">
+    <h3>Race and Ethnicity Breakdown</h3>
     <ResponsivePieCanvas
         data={race_ethnicity_data}
         margin={{ top: 40, right: 200, bottom: 40, left: 80 }}
         pixelRatio={2}
         innerRadius={0.5}
-        padAngle={0.7}
-        cornerRadius={3}
-        colors={{ scheme: 'paired' }}
+        colors={[
+          "#FFF59D",
+          '#F4511E',
+          "#DCE775",
+          "#8BC34A",
+          "#00796B",
+          "#006064"
+        ]}
         borderColor={{ from: 'color', modifiers: [ [ 'darker', 0.6 ] ] }}
         radialLabelsSkipAngle={10}
         radialLabelsTextXOffset={6}
@@ -35,76 +40,6 @@ const RaceEthnicityPie = props => {
         animate={true}
         motionStiffness={90}
         motionDamping={15}
-        defs={[
-            {
-                id: 'dots',
-                type: 'patternDots',
-                background: 'inherit',
-                color: 'rgba(255, 255, 255, 0.3)',
-                size: 4,
-                padding: 1,
-                stagger: true
-            },
-            {
-                id: 'lines',
-                type: 'patternLines',
-                background: 'inherit',
-                color: 'rgba(255, 255, 255, 0.3)',
-                rotation: -45,
-                lineWidth: 6,
-                spacing: 10
-            }
-        ]}
-        fill={[
-            {
-                match: {
-                    id: 'asian'
-                },
-                id: 'dots'
-            },
-            {
-                match: {
-                    id: 'c'
-                },
-                id: 'dots'
-            },
-            {
-                match: {
-                    id: 'go'
-                },
-                id: 'dots'
-            },
-            {
-                match: {
-                    id: 'python'
-                },
-                id: 'dots'
-            },
-            {
-                match: {
-                    id: 'scala'
-                },
-                id: 'lines'
-            },
-            {
-                match: {
-                    id: 'lisp'
-                },
-                id: 'lines'
-            },
-            {
-                match: {
-                    id: 'elixir'
-                },
-                id: 'lines'
-            },
-            {
-                match: {
-                    id: 'javascript'
-                },
-                id: 'lines'
-            }
-        ]}
         legends={[
             {
                 anchor: 'right',
